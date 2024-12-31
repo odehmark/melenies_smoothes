9e4131f5b2e3ef6b6ee881876b524f06be23a438
Ode Mark Alogwu <58354670+odehmark@users.noreply.github.com>
2024-12-31 19:27:25 +1000

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -18,9 +18,9 @@ st.write(
 name_on_order  = st.text_input('Name of Smoothie:')
 st.write('The name of your smoothie will be', name_on_order )
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-session = get_active_session()
+conn = st.connection("snowflake")
+session = conn.session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
