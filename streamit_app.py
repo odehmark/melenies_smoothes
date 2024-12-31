69ea2b70dc69b8299b0ff5afe4d7d5035188dde6
Ode Mark Alogwu <58354670+odehmark@users.noreply.github.com>
2024-12-31 19:13:01 +1000

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -18,7 +18,6 @@ session = cnx.session()
 name_on_order  = st.text_input('Name of Smoothie:')
 st.write('The name of your smoothie will be', name_on_order )
 
-#def get_active_session():
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
