edd9072a510e4bc50751c3c515b918d202112807
Ode Mark Alogwu <58354670+odehmark@users.noreply.github.com>
2024-12-31 20:36:23 +1000

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 cnx = st.connection("snowflake")
 session = cnx.session()Put code here
-` ` `
 #session = get_active_session()
 #cnx = st.connection("snowflake")
 #session = cnx.session()
